38d8d0892aabaddd226b86cac55662a962ec5d05
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-12 02:36:41 +0100

    js

---
+++ quiz.js
@@ -0,0 +1,69 @@
+const data = [
+  {
+    question: "Who is the current ballon d'or winner?",
+    a: "Erling Haaland",
+    b: "Virgil Van dijk",
+    c: "Lionel messi",
+    d: "Vinicius jr",
+    correct: "c",
+  },
+  {
+    question: "Which Nigerian artist won the Grammy Award in 2021?",
+    a: "Burna Boy",
+    b: "Wizkid",
+    c: "Davido",
+    d: "Tiwa Savage",
+    correct: "a",
+  },
+  {
+    question:
+      "Who is the lead actor in the Nigerian movie 'The Wedding Party'?",
+    a: "RMD",
+    b: "Banky W",
+    c: "Adesua Etomi",
+    d: "Sola Sobowale",
+    correct: "b",
+  },
+  {
+    question: "What is the name of Wizkid's debut album?",
+    a: "Superstar",
+    b: "Ayo",
+    c: "Sounds from the Other Side",
+    d: "Made in Lagos",
+    correct: "a",
+  },
+  {
+    question: "Css is an type of ____ Language ?",
+    a: "Back-end",
+    b: "ML",
+    c: "Front-end",
+    d: "Gaming",
+    correct: "c",
+  },
+  {
+    question:
+      "Who is the host of the Nigerian TV show 'Who Wants to Be a Millionaire'?",
+    a: "Frank Edoho",
+    b: "Ebuka Obi-Uchendu",
+    c: "Ik Osakioduwa",
+    d: "Denrele Edun",
+    correct: "a",
+  },
+  {
+    question: "Who is the current Governor of Lagos State?",
+    a: "Babajide Sanwo-Olu",
+    b: "Akinwunmi Ambode",
+    c: "Bola Tinubu",
+    d: "Rauf Aregbesola",
+    correct: "a",
+  },
+  {
+    question:
+      "Which state in Nigeria has the highest number of local governments?",
+    a: "Kano",
+    b: "Lagos",
+    c: "Kaduna",
+    d: "Rivers",
+    correct: "a",
+  },
+];
